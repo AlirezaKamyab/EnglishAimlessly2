3487d1905681a6c2e46570127fae108a9feb2d82
AlirezaKamyab <alirezakamyab19@gmail.com>
2021-10-16 07:57:18 +0330

    Bug fixs

--- EnglishAimlessly2/obj/Release/netcoreapp3.1/UserControls/GroupItemControl.g.i.cs
+++ EnglishAimlessly2/obj/Release/netcoreapp3.1/UserControls/GroupItemControl.g.i.cs
@@ -64,13 +64,13 @@ namespace EnglishAimlessly2.UserControls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EnglishAimlessly2;V1.0.1.0;component/usercontrols/groupitemcontrol.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EnglishAimlessly2;V2.0.0.0;component/usercontrols/groupitemcontrol.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\UserControls\GroupItemControl.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -80,7 +80,7 @@ namespace EnglishAimlessly2.UserControls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
